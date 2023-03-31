c95c7e863f48b79dd32137a11772b894bac84c8d
Andarias <andariasito@gmail.com>
2023-03-31 23:28:41 +0700

    [Test] Sumbit Test

--- src/com/pertamina/tes/First.java
+++ src/com/pertamina/tes/First.java
@@ -2,19 +2,6 @@ package com.pertamina.tes;
 
 public class First {
     public static void main(String[] args) {
-//        for (int i = 1; i <= 3; i++) {
-//            for (int j = 1; j <= 5; j++) {
-//                System.out.println();
-//                if (j == 5) {
-//                    System.out.println();
-//                }
-//                for (int k = 1; k <= 1; k++) {
-//                    for (int l = 5; l >= 1; l--) {
-//                        System.out.print(l);
-//                    }
-//                }
-//            }
-//        }
         for (int i = 1; i <= 3; i++){
             for (int j = 1; j <= 5; j++) {
                 System.out.print(j);
